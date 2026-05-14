96e9cab434f03d83c5a2c3e9f6e8357ded683df6
MOHAMMED ADEL <ffjjddsskkjj78@gmail.com>
2026-05-14 21:58:57 +0200

    Update engine.py

--- engine.py
+++ engine.py
@@ -11,42 +11,37 @@ class SUSTEngine:
 
     def login(self, user, pw):
         try:
-            # الحصول على توكن تسجيل الدخول من مودل
             res = self.session.get(f"{self.base_url}/login/index.php")
             soup = BeautifulSoup(res.text, 'html.parser')
             token = soup.find('input', {'name': 'logintoken'})['value']
             
             data = {'username': user, 'password': pw, 'logintoken': token}
             post_res = self.session.post(f"{self.base_url}/login/index.php", data=data)
-            
-            # إذا ظهر رابط الخروج، يعني الدخول نجح
             return "login/logout.php" in post_res.text
         except:
             return False
 
     def get_courses(self):
-        """سحب المواد المسجلة للطالب تلقائياً"""
-        res = self.session.get(f"{self.base_url}/my/")
-        soup = BeautifulSoup(res.text, 'html.parser')
-        courses = []
-        # البحث عن روابط المواد بالـ Selectors الخاصة بـ SUST
-        for a in soup.select('.coursename a') or soup.select('h4.multiline a'):
-            courses.append({
-                'name': a.get_text(strip=True),
-                'id': a['href'].split('id=')[-1]
-            })
-        return courses
+        try:
+            res = self.session.get(f"{self.base_url}/my/")
+            soup = BeautifulSoup(res.text, 'html.parser')
+            courses = []
+            for a in soup.select('.coursename a') or soup.select('h4.multiline a'):
+                courses.append({
+                    'name': a.get_text(strip=True),
+                    'id': a['href'].split('id=')[-1]
+                })
+            return courses
+        except: return []
 
     def get_videos(self, course_id):
-        """سحب روابط الفيديوهات داخل مادة معينة"""
-        url = f"{self.base_url}/course/view.php?id={course_id}"
-        res = self.session.get(url)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        videos = []
-        for a in soup.find_all('a', href=True):
-            if '.mp4' in a['href'] or 'video' in a['href']:
-                videos.append({
-                    'title': a.get_text(strip=True) or "فيديو محاضرة",
-                    'url': a['href']
-                })
-        return videos
+        try:
+            url = f"{self.base_url}/course/view.php?id={course_id}"
+            res = self.session.get(url)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            videos = []
+            for a in soup.find_all('a', href=True):
+                if '.mp4' in a['href'] or 'video' in a['href']:
+                    videos.append({'title': a.get_text(strip=True) or "فيديو محاضرة", 'url': a['href']})
+            return videos
+        except: return []
